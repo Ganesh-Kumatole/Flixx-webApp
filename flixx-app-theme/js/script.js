4903b17850f205e32c6205bc720e7c9386fd3375
GaneshKumatole <workspace.ganeshkumatole@gmail.com>
2025-06-24 23:24:47 +0530

    Add Search Functionality

--- flixx-app-theme/js/script.js
+++ flixx-app-theme/js/script.js
@@ -1,7 +1,18 @@
-const currentPath = location.pathname;
+const global = {
+  currentPath: location.pathname,
+  optionsGET: {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjA2ZGZhM2Y1ZWE5MjJlMGE1YmNkNGRiMDhkNDI4NyIsIm5iZiI6MTc1MDI2ODA1MC43MTgwMDAyLCJzdWIiOiI2ODUyZjg5MjY5OTFiNjU2Yjg1ZDg1MGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZpGd7-AvNcTATX8y3fHhPxUJnCI6oInuBYmTfsnUbqs",
+    },
+  },
+  page: 1,
+};
 
 // Add commas to Bignumber
-function addCommas(number) {
+function addCommastoNumber(number) {
   const digits = number.toString().split("").reverse();
   const withCommas = [];
 
@@ -15,20 +26,15 @@ function addCommas(number) {
   return withCommas.reverse().join("");
 }
 
-// GET Request for an endpoint
 async function fetchData(endpoint) {
   try {
     // Show spinner
     document.querySelector("div.spinner").classList.add("show");
 
-    const response = await fetch(`https://api.themoviedb.org/3${endpoint}`, {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZjA2ZGZhM2Y1ZWE5MjJlMGE1YmNkNGRiMDhkNDI4NyIsIm5iZiI6MTc1MDI2ODA1MC43MTgwMDAyLCJzdWIiOiI2ODUyZjg5MjY5OTFiNjU2Yjg1ZDg1MGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZpGd7-AvNcTATX8y3fHhPxUJnCI6oInuBYmTfsnUbqs",
-      },
-    });
+    const response = await fetch(
+      `https://api.themoviedb.org/3${endpoint}`,
+      global.optionsGET
+    );
     if (response.ok) {
       const data = await response.json();
 
@@ -42,6 +48,137 @@ async function fetchData(endpoint) {
   } catch (error) {
     throw new Error(error);
   }
+}
+
+async function fetchData_forSearch(searchEndpoint, optionsObj) {
+  // Show spinner
+  document.querySelector("div.spinner").classList.add("show");
+
+  const response = await fetch(searchEndpoint, optionsObj);
+  if (response.ok) {
+    const data = await response.json();
+
+    // Hide spinner
+    document.querySelector("div.spinner").classList.remove("show");
+
+    return data;
+  } else {
+    throw new Error(`HTTP Error: ${response.status}`);
+  }
+}
+
+// Custom Alert Pop-up
+async function showAlert(alertMessage) {
+  const alert = document.querySelector("div#alert");
+  alert.textContent = alertMessage;
+  alert.className = "alert-error";
+  await setTimeout(() => {
+    alert.remove();
+  }, 3000);
+}
+
+// Search Movies/TvShows
+async function search(movieOrTv, searchQuery, currentPage) {
+  // Retain checked radio button
+  let movieSelected = false;
+  if (movieOrTv === "movie") {
+    movieSelected = true;
+    document.querySelector("input#movie").checked = true;
+  } else {
+    document.querySelector("input#tv").checked = true;
+  }
+
+  const { results, total_pages, total_results } = await fetchData_forSearch(
+    `https://api.themoviedb.org/3/search/${movieOrTv}?query=${searchQuery}&page=${currentPage}`,
+    global.optionsGET
+  );
+
+  if (results.length === 0) {
+    showAlert("No Results were found!");
+    return;
+  }
+
+  // Clear previous pagination results
+  document.querySelector("div#search-results").innerHTML = "";
+
+  // Grid Heading
+  document.querySelector(
+    "h2#search-results-heading"
+  ).innerText = `Showing ${results.length} Results Out Of ${total_results}`;
+
+  // Displaying the results into the DOM
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+    <a href="${movieOrTv}-details.html?id=${result.id}">
+      ${
+        result.poster_path
+          ? `<img src="https://media.themoviedb.org/t/p/w440_and_h660_face${
+              result.poster_path
+            }"
+         class="card-img-top"
+         alt="${movieSelected ? result.title : result.name}"
+       />`
+          : `<img
+         src="images/no-image.jpg"
+         class="card-img-top"
+         alt="${movieSelected ? result.title : result.name}"
+       />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">
+      ${movieSelected ? result.title : result.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">
+        ${
+          movieSelected
+            ? `<strong>Release: </strong> ${result.release_date}`
+            : `<strong>Air Date: </strong> ${result.first_air_date}`
+        }</small>
+      </p>
+    </div>
+    `;
+
+    document.querySelector("div#search-results").append(div);
+  });
+
+  document.querySelector("input#search-term").value = searchQuery;
+
+  pagination(currentPage, total_pages, movieOrTv, searchQuery);
+}
+
+// Pagination
+function pagination(currentPage, total_pages, movieOrTv, searchQuery) {
+  // Display Pagination
+  document.querySelector("div#pagination").innerHTML = `
+    <div class="pagination">
+      <button class="btn btn-primary" id="prev">Prev</button>
+      <button class="btn btn-primary" id="next">Next</button>
+      <div class="page-counter">Page ${currentPage} of ${total_pages}</div>
+    </div>
+  `;
+
+  const prev = document.querySelector("button#prev");
+  const next = document.querySelector("button#next");
+
+  // Disable prev/next btn for extreme pages
+  if (currentPage === 1) {
+    prev.disabled = true;
+  }
+  if (currentPage === total_pages) {
+    next.disabled = true;
+  }
+
+  // Attaching events to prev/next btn
+  prev.addEventListener("click", () => {
+    search(movieOrTv, searchQuery, currentPage - 1);
+  });
+
+  next.addEventListener("click", () => {
+    search(movieOrTv, searchQuery, currentPage + 1);
+  });
 }
 
 // List popular movies
@@ -70,7 +207,6 @@ async function popularMovies(endpoint) {
          alt="${movie.title}"
        />`
       }
-       
      </a>
      <div class="card-body">
        <h5 class="card-title">${movie.title}</h5>
@@ -163,10 +299,10 @@ async function movieDetails(movieId) {
   document.querySelector("div.details-bottom").innerHTML = `
   <h2>Movie Info</h2>
     <ul>
-      <li><span class="text-secondary">Budget:</span> $${addCommas(
+      <li><span class="text-secondary">Budget:</span> $${addCommastoNumber(
         data.budget
       )}</li>
-      <li><span class="text-secondary">Revenue:</span> $${addCommas(
+      <li><span class="text-secondary">Revenue:</span> $${addCommastoNumber(
         data.revenue
       )}</li>
       <li><span class="text-secondary">Runtime:</span> ${
@@ -241,7 +377,7 @@ async function tvShowDetails(tvShowId) {
 // Intialize web-app
 function init() {
   // Kinda Page router
-  switch (currentPath) {
+  switch (global.currentPath) {
     case "/":
 
     case "/index.html":
@@ -265,15 +401,26 @@ function init() {
       break;
 
     case "/search.html":
-      console.log("Search page");
+      const queryParams = new URLSearchParams(location.search);
+      if (queryParams.get("type") === null) {
+        showAlert("Please select an option: Movies or Tv Shows");
+      } else if (queryParams.get("searchTerm") === "") {
+        showAlert("Please Enter the Search Term!");
+      } else {
+        search(
+          queryParams.get("type"),
+          queryParams.get("searchTerm"),
+          global.page
+        );
+      }
       break;
   }
 
   // Highlight active nav-link
-  if (currentPath === "/" || currentPath === "/index.html") {
+  if (global.currentPath === "/" || global.currentPath === "/index.html") {
     const moviesLink = document.querySelector(".movies");
     moviesLink.classList.add("active");
-  } else if (currentPath === "/shows.html") {
+  } else if (global.currentPath === "/shows.html") {
     const showsLink = document.querySelector(".tv-shows");
     showsLink.classList.add("active");
   }
